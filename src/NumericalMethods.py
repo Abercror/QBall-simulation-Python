13a96a106fa5761ec338d6250ccb2c9f6b99728e
Rhys Abercromby <rhys.abercromby@gmail.com>
2025-11-04 23:42:37 +0000

    corrected RungeKutta4SecondOrder function

--- src/NumericalMethods.py
+++ src/NumericalMethods.py
@@ -1,26 +1,27 @@
 class NumericalMethods:
-    def rungeKuttaSecondOrder(self, x, y, yDerivative, delta, secondOrderDifferentialY, *args, **kwargs):
+    def rungeKutta4SecondOrder(self, x, y, yDerivative, delta, secondOrderDifferentialY, *args, **kwargs):
         x0 = x
         y0 = y
+        dy = yDerivative
    
-        k0 = yDerivative
-        a0 = secondOrderDifferentialY(x0, y0, k0, *args, **kwargs)
+        k1 = dy
+        a1 = secondOrderDifferentialY(x0, y0, dy, *args, **kwargs)
 
-        k1 = k0 + a0*(delta/2)
-        a1 = secondOrderDifferentialY(x0 + delta/2, y0 + k0 * delta/2, k1, *args, **kwargs)
+        k2 = dy + a1 * (delta/2)
+        a2 = secondOrderDifferentialY(x0 + delta/2, y0 + k1 * delta/2, dy + (delta/2) * a1, *args, **kwargs)
 
-        k2 = k1 + a1*(delta/2)
-        a2 = secondOrderDifferentialY(x0 + delta/2, y0 + k1 * delta/2, k2, *args, **kwargs)
+        k3 = dy + a2 * (delta/2)
+        a3 = secondOrderDifferentialY(x0 + delta/2, y0 + k2 * delta/2, dy + (delta/2) * a2, *args, **kwargs)
 
-        k3 = k2 + a2*delta
-        a3 = secondOrderDifferentialY(x0 + delta, y0 + k2 * delta, k3, *args, **kwargs)
+        k4 = dy + a3 * delta
+        a4 = secondOrderDifferentialY(x0 + delta, y0 + k3 * delta, dy + delta * a3, *args, **kwargs)
 
-        y += (delta/6) * (k0 + 2*k1 + 2*k2 + k3)
-        yDerivative += (delta/6) * (a0 + 2*a1 + 2*a2 + a3)
+        y = y0 + (delta/6) * (k1 + 2*k2 + 2*k3 + k4)
+        yDerivative = dy + (delta/6) * (a1 + 2*a2 + 2*a3 + a4)
 
         return y, yDerivative
     
-    def rungeKuttaFirstOrder(self, x, y, delta, differentialY, *args, **kwargs):
+    def rungeKutta4FirstOrder(self, x, y, delta, differentialY, *args, **kwargs):
         y0 = y
         x0 = x
 
